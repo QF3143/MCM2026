40be48497b97e3648f789a555a08c65e2edd8219
sunset Motel <lqfatsjtu@sjtu.edu.cn>
2026-02-01 13:33:35 +0800

    完成敏感性分析的图谱

--- QF‘s solution/Bayes_Elo/bayes_elo.py
+++ QF‘s solution/Bayes_Elo/bayes_elo.py
@@ -453,7 +453,7 @@ class BayesianEloEstimator:
         else:
             f_pct = f_pct / f_pct.sum()
         
-        use_ranking_rule = False#(season <= 2 or season > 27)
+        use_ranking_rule = (season <= 2 or season > 27)#false
 
         # [修改] 调用蒙特卡洛函数，传入 use_ranking_rule
         death_counts, avg_scores = self._mc_func(
